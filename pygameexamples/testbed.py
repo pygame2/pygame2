2f1b95daaccc968df342f25b83df3eedf48ad89e
bitcraft <leif.theden@gmail.com>
2015-03-23 00:45:34 -0500

    testbed animations

--- pygameexamples/testbed.py
+++ pygameexamples/testbed.py
@@ -5,6 +5,7 @@ the general goal is to create new useful functions for pygame2
 this testbed is meant to code first, then refactor useful code
 into pygame2.
 """
+from functools import partial
 import os
 import pygame2
 
@@ -24,20 +25,6 @@ def main():
     window = app.create_window(size=size)
     renderer = window.create_renderer()
 
-    path = os.path.join('resources', 'pygame2-nologo.png')
-    texture0 = pygame2.core.image.load(path).create_texture()
-
-    path = os.path.join('resources', 'pygame2.png')
-    texture1 = pygame2.core.image.load(path).create_texture()
-
-    for i in range(4):
-        if i % 2 == 0:
-            sprite = renderer.create_sprite(texture=texture0)
-        else:
-            sprite = renderer.create_sprite(texture=texture1)
-
-        sprite.rotation = i * (360 / 4)
-
     def on_draw(*args, **kwargs):
         window.clear()
         renderer.draw()
@@ -46,13 +33,64 @@ def main():
     window.bind('on_key_press', on_key_press)
     window.bind('on_mouse_motion', on_mouse_motion)
 
-    def update(dt):
-        for i, sprite in enumerate(renderer.sprites()):
-            sprite.rect.width += .1 * dt
-            sprite.rect.height += .05 * dt
-            sprite.rotation += 20 * i * dt
-            sprite.update_transform()
+    path = os.path.join('resources', 'pygame2-nologo.png')
+    texture0 = pygame2.core.image.load(path).create_texture()
 
+    path = os.path.join('resources', 'pygame2.png')
+    texture1 = pygame2.core.image.load(path).create_texture()
+
+    animations = list()
+
+    for i in range(4):
+        if i % 2 == 0:
+            sprite = renderer.create_sprite(texture=texture0)
+        else:
+            sprite = renderer.create_sprite(texture=texture1)
+
+    def offset(*args):
+        for i, sprite in enumerate(renderer.sprites()):
+            ani = pygame2.animation.Animation(rotation=i * (360 / 8))
+            ani.update_callback = sprite.update_transform
+            ani.callback = partial(animations.remove, ani)
+            ani.start(sprite)
+            animations.append(ani)
+
+    def grow(*args):
+        for sprite in renderer.sprites():
+            ani = pygame2.animation.Animation(width=2, duration=4)
+            ani.update_callback = sprite.update_transform
+            ani.callback = partial(animations.remove, ani)
+            ani.start(sprite.rect)
+            animations.append(ani)
+
+    def shrink(*args):
+        for sprite in renderer.sprites():
+            ani = pygame2.animation.Animation(width=1, duration=1)
+            ani.update_callback = sprite.update_transform
+            ani.callback = partial(animations.remove, ani)
+            ani.start(sprite.rect)
+            animations.append(ani)
+
+    def reset_rotation(*args):
+        for sprite in renderer.sprites():
+            ani = pygame2.animation.Animation(rotation=0)
+            ani.update_callback = sprite.update_transform
+            ani.callback = partial(animations.remove, ani)
+            ani.start(sprite)
+            animations.append(ani)
+
+    def update(dt):
+        for ani in animations:
+            ani.update(dt)
+
+    def boo_ya_ka_sha(*args):
+        offset()
+        app.clock.schedule(grow, 1.5)
+        app.clock.schedule(shrink, 5.5)
+        app.clock.schedule(reset_rotation, 5.5)
+
+    boo_ya_ka_sha()
+    app.clock.schedule(boo_ya_ka_sha, 7, repeat=True)
     app.clock.schedule(update, 1 / 70., repeat=True)
 
     # this will start the application and will exit when window is closed
